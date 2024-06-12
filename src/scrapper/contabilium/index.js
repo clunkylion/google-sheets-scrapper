0003ba7dc42a6df15a4fbab1be2d8f89d2c400f4
Jose Cortes <cote99salamanca@gmail.com>
2024-06-12 11:55:55 -0400

    add aws-lambda-chrome for production

--- src/scrapper/contabilium/index.js
+++ src/scrapper/contabilium/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import chromium from 'chrome-aws-lambda';
 //import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { setupPuppeteer } from './puppeteerConfig.js';
 import { login } from './auth.js';
@@ -12,11 +13,14 @@ const contabiliumScrapper = async () => {
 
   const browser = await puppeteer.launch({
     args: [
+      ...chromium.args,
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--single-process',
       '--no-zygote',
     ],
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
   const page = await browser.newPage();
 

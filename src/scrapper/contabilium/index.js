177fccf99a78339710c664d3a204353650c916fc
Jose Cortes <cote99salamanca@gmail.com>
2024-06-12 13:17:51 -0400

    fix scrapper

--- src/scrapper/contabilium/index.js
+++ src/scrapper/contabilium/index.js
@@ -1,6 +1,5 @@
 import 'dotenv/config';
-import puppeteer from 'puppeteer-core';
-import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer';
 //import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { setupPuppeteer } from './puppeteerConfig.js';
 import { login } from './auth.js';
@@ -13,14 +12,11 @@ const contabiliumScrapper = async () => {
 
   const browser = await puppeteer.launch({
     args: [
-      ...chromium.args,
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--single-process',
       '--no-zygote',
     ],
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
   });
   const page = await browser.newPage();
 
